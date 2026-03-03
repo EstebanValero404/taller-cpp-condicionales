7d37e715b431cd4635b6db8757defdf3a3c2f374
EstebanValero404 <esteban2008games@gmail.com>
2026-03-03 15:56:47 -0500

    ejercisio3.cpp
    permisos en bits

--- ejercisio3.cpp
+++ ejercisio3.cpp
@@ -1,21 +1,21 @@
-// ejercicio_03.cpp
 #include <iostream>
-int main() {
+
+int main()
+{
     const int LEER = 0b001;
     const int ESCRIBIR = 0b010;
     const int EJECUTAR = 0b100;
 
     int valor;
 
-    cout << "Ingrese el valor de permisos (0-7): ";
-    cin >> valor;
+    std::cout << "Ingrese el valor de permisos (0-7): " << std::endl;
+    std::cin >> valor;
 
     if (valor & LEER)
-        cout << "Tiene permiso de LEER" << endl;
+        std::cout << "Tiene permiso de LEER" << std::endl;
     if (valor & ESCRIBIR)
-        cout << "Tiene permiso de ESCRIBIR" << endl;
+        std::cout << "Tiene permiso de ESCRIBIR" << std::endl;
     if (valor & EJECUTAR)
-        cout << "Tiene permiso de EJECUTAR" << endl;
-
+        std::cout << "Tiene permiso de EJECUTAR" << std::endl;
     return 0;
 }
